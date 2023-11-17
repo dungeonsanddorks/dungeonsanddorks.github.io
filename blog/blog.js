fe46cefc416d1ef70a759df759b8a81d67b3eefe
asdfgn2399 <alexannett88@gmail.com>
2023-11-16 19:43:26 -0800

    Testing Blog

--- blog/blog.js
+++ blog/blog.js
@@ -1,14 +1,47 @@
 class Blog {
-	init () {
-		this.loadPosts()
+	async initBlogPage () {
+		await this.loadPosts()
+		this.setUpBlogPageList()
 	}
 
 	async loadPosts () {
 		this.postData = await fetch('/blog/post-data/posts.json').then(response => response.json())
 		this.posts = this.postData.posts
-		console.log(this.posts)
+	}
+
+	setUpBlogPageList () {
+		this.output = "";
+		this.posts.forEach(post => {
+			let date = new Date(post.dateLastModified * 1000)
+			this.output += `<div class="uc_post_list_box">
+	<div class="uc_post_list_image">
+		<a href="https://dungeonsanddorks.github.io/" target="_self" title="${post.title}">
+			<img decoding="async" src="https://raw.githubusercontent.com/dungeonsanddorks/dungeonsanddorks.github.io/main/blog/post-data/images/${post.image}" alt="${post.imageAlt}" width="768" height="576">
+		</a>
+	</div>
+	<div class="uc_post_list_content">
+		<div class="uc_post_list_content_inside">
+			<div class="uc_post_list_title">
+				<a target="_self" href="https://dungeonsanddorks.github.io/" title="${post.title}">
+					${post.title}
+				</a>
+			</div>	
+			<div class="ue-meta-data">
+				<div class="ue-grid-item-meta-data">
+					<span class="ue-grid-item-meta-data-icon"> <i class="fas fa-calendar-week"></i></span> ${date.toLocaleString('default', { month: 'long' })} ${date.getDate()}, ${date.getFullYear()}
+				</div>                                   
+			</div>
+			<div class="uc_post_content">
+				${post.exerpt}
+			</div>      
+		</div>
+	</div>
+</div>
+`
+		});
+		document.getElementById("blog-2-blog-post-list").innerHTML = this.output;
 	}
 }
 
 const blog = new Blog
-blog.init()
+blog.initBlogPage()
