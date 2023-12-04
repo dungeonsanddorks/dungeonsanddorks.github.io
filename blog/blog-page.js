5df63ded3a8895f9588b659675fb6e5e1b7c7fe6
asdfgn2399 <alexannett88@gmail.com>
2023-12-03 21:54:09 -0800

    Testing Comments

--- blog/blog-page.js
+++ blog/blog-page.js
@@ -1,7 +1,7 @@
-async function loadPosts() {
-	globalThis.postData = await fetch('/blog/post-data/posts.json').then(response => response.json())
-	globalThis.posts = this.postData.posts
-	globalThis.posts.sort((a,b) => b.dateLastModified - a.dateLastModified);
+async function loadData() {
+	let [posts, comments] = await Promise.all([fetch('/blog/post-data/posts.json').then(response => response.json()), fetch('/blog/comment-data/comments.json').then(response => response.json())]);
+	globalThis.posts = posts.posts.sort((a,b) => b.dateLastModified - a.dateLastModified);
+	globalThis.comments = comments.comments;
 }
 
 function renderPost(post) {
@@ -14,7 +14,7 @@ function renderPost(post) {
 	var output = `<header class="entry-header">
 	<h1 class="entry-title" itemprop="headline">${post.title}</h1>
 	<div class="entry-meta">
-	` +  /* TODO: Comments <span class="comments-link"><a href="https://dungeonsanddorks.github.io/blog/?post=${post.id}#respond">Leave a Comment</a></span> / */ /* TODO: Catagoreies <span class="ast-terms-link"><a href="https://dungeonsanddorks.github.io/category/uncategorized/">Uncategorized</a></span> / */ `By <span class="posted-by vcard author" itemtype="https://schema.org/Person" itemscope="itemscope" itemprop="author"><a title="View all posts by ${post.author}" href="https://dungeonsanddorks.github.io/author/${post.author.replaceAll(" ", "-").toLowerCase()}/" rel="author" class="url fn n" itemprop="url"><span class="author-name" itemprop="name">${post.author}</span> </a> </span> 
+	` +  /* TODO: Comments  */ /* TODO: Catagoreies <span class="ast-terms-link"><a href="https://dungeonsanddorks.github.io/category/uncategorized/">Uncategorized</a></span> / */ `By <span class="posted-by vcard author" itemtype="https://schema.org/Person" itemscope="itemscope" itemprop="author"><a title="View all posts by ${post.author}" href="https://dungeonsanddorks.github.io/author/${post.author.replaceAll(" ", "-").toLowerCase()}/" rel="author" class="url fn n" itemprop="url"><span class="author-name" itemprop="name">${post.author}</span> </a> </span> 
 	</div>
 	${imageHTML}
 	</header>
@@ -26,6 +26,11 @@ function renderPost(post) {
 
 	// Render Navigation Links
 	document.getElementsByClassName("nav-links")[0].innerHTML = checkForNavigation(post);
+
+	// Render Comments
+	const renderedComments = renderComments(post)
+	if (renderedComments.title) document.getElementById("comments").innerHTML += `<div class="comments-count-wrapper"><h3 class="comments-title"></h3>${renderedComments.title}</h3></div>`
+	if (renderedComments.comments) document.getElementById("comments").innerHTML += `<ol class="ast-comment-list">${renderedComments.comments}</ol>`
 } 
 
 function postRenderer(lineArr, depth) {
@@ -78,11 +83,28 @@ function checkForNavigation(post) {
 	return output
 }
 
+function renderComments(post) {
+	var comments = globalThis.comments.filter(obj => obj.postID == post.id)
+	
+	if (comments.length == 0) {
+		document.getElementsByClassName("entry-meta")[0].innerHTML = `<span class="comments-link"><a href="https://dungeonsanddorks.github.io/blog/?post=${post.id}#respond">Leave a Comment</a></span> /` + document.getElementsByClassName("entry-meta")[0].innerHTML;
+		return { comments: "", title: "" };
+	} else if (comments.length == 1) {
+		document.getElementsByClassName("entry-meta")[0].innerHTML = `<span class="comments-link"><a href="https://dungeonsanddorks.github.io/blog/?post=${post.id}#respond">1 Comment</a></span> /` + document.getElementsByClassName("entry-meta")[0].innerHTML;
+		return { comments: "", title: `1 thought on “${post.title}”` }
+	} else {
+		document.getElementsByClassName("entry-meta")[0].innerHTML = `<span class="comments-link"><a href="https://dungeonsanddorks.github.io/blog/?post=${post.id}#respond">${comments.length} Comments</a></span> /` + document.getElementsByClassName("entry-meta")[0].innerHTML;
+	}
+	
+	var output = ""
+	
+	return { comments: output, title: `${comments.length} thoughts on “${post.title}”` }
+}
+
 async function init() {
 	const queryString = window.location.search
 	const urlParams = new URLSearchParams(queryString);
-	var currentPost = {};
-	await loadPosts()
+	await loadData()
 	
 	const page = urlParams.has('post') ? urlParams.get('post').split('#')[0] : 0;
 	const result = globalThis.posts.find(obj => {
