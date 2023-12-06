57188bcd318778dfb3eb742c00efb1342667945b
asdfgn2399 <alexannett88@gmail.com>
2023-12-05 21:35:41 -0800

    Used a Prettifier

--- blog/blog-page.js
+++ blog/blog-page.js
@@ -1,120 +1,173 @@
 async function loadData() {
-	let [posts, comments] = await Promise.all([fetch('/blog/post-data/posts.json').then(response => response.json()), fetch('/blog/comment-data/comments.json').then(response => response.json())]);
-	globalThis.posts = posts.posts.sort((a,b) => b.dateLastModified - a.dateLastModified);
-	globalThis.comments = comments.comments;
+  let [posts, comments] = await Promise.all([
+    fetch("/blog/post-data/posts.json").then((response) => response.json()),
+    fetch("/blog/comment-data/comments.json").then((response) =>
+      response.json()
+    ),
+  ]);
+  globalThis.posts = posts.posts.sort(
+    (a, b) => b.dateLastModified - a.dateLastModified
+  );
+  globalThis.comments = comments.comments;
 }
 
 function renderPost(post) {
-	// Render Post Header
-	var imageHTML = ""
-	if (post.image !== "placeholder.jpeg") imageHTML = `<div class="post-thumb-img-content post-thumb">
+  // Render Post Header
+  var imageHTML = "";
+  if (post.image !== "placeholder.jpeg")
+    imageHTML = `<div class="post-thumb-img-content post-thumb">
 		<img width="1024" height="768" src="https://raw.githubusercontent.com/dungeonsanddorks/dungeonsanddorks.github.io/main/blog/post-data/images/${post.image}" class="attachment-large size-large wp-post-image" alt="${post.imageAlt}" itemprop="image" decoding="async" sizes="(max-width: 1024px) 100vw, 1024px">
 	</div>
-	`
-	var output = `<header class="entry-header">
+	`;
+  var output =
+    `<header class="entry-header">
 	<h1 class="entry-title" itemprop="headline">${post.title}</h1>
 	<div class="entry-meta">
-	` +  /* TODO: Comments  */ /* TODO: Catagoreies <span class="ast-terms-link"><a href="https://dungeonsanddorks.github.io/category/uncategorized/">Uncategorized</a></span> / */ `By <span class="posted-by vcard author" itemtype="https://schema.org/Person" itemscope="itemscope" itemprop="author"><a title="View all posts by ${post.author}" href="https://dungeonsanddorks.github.io/author/${post.author.replaceAll(" ", "-").toLowerCase()}/" rel="author" class="url fn n" itemprop="url"><span class="author-name" itemprop="name">${post.author}</span> </a> </span> 
+	` +
+    /* TODO: Comments  */ /* TODO: Catagoreies <span class="ast-terms-link"><a href="https://dungeonsanddorks.github.io/category/uncategorized/">Uncategorized</a></span> / */ `By <span class="posted-by vcard author" itemtype="https://schema.org/Person" itemscope="itemscope" itemprop="author"><a title="View all posts by ${
+      post.author
+    }" href="https://dungeonsanddorks.github.io/author/${post.author
+      .replaceAll(" ", "-")
+      .toLowerCase()}/" rel="author" class="url fn n" itemprop="url"><span class="author-name" itemprop="name">${
+      post.author
+    }</span> </a> </span> 
 	</div>
 	${imageHTML}
 	</header>
-`
-	document.getElementsByClassName("ast-post-format-")[0].innerHTML += output;
+`;
+  document.getElementsByClassName("ast-post-format-")[0].innerHTML += output;
 
-	// Render Post Content
-	document.getElementsByClassName("ast-post-format-")[0].innerHTML += '<div class="entry-content clear" itemprop="text">' + postRenderer(post.entries, 0) + '</div>';
+  // Render Post Content
+  document.getElementsByClassName("ast-post-format-")[0].innerHTML +=
+    '<div class="entry-content clear" itemprop="text">' +
+    postRenderer(post.entries, 0) +
+    "</div>";
 
-	// Render Navigation Links
-	document.getElementsByClassName("nav-links")[0].innerHTML = checkForNavigation(post);
+  // Render Navigation Links
+  document.getElementsByClassName("nav-links")[0].innerHTML =
+    checkForNavigation(post);
 
-	// Render Comments
-	const renderedComments = renderComments(post)
-	if (renderedComments.title) document.getElementById("comments").innerHTML += `<div class="comments-count-wrapper"><h3 class="comments-title">${renderedComments.title}</h3></div>`
-	if (renderedComments.comments) document.getElementById("comments").innerHTML += `<ol class="ast-comment-list">${renderedComments.comments}</ol>`
-} 
+  // Render Comments
+  const renderedComments = checkForComments(post);
+  if (renderedComments.title)
+    document.getElementById(
+      "comments"
+    ).innerHTML += `<div class="comments-count-wrapper"><h3 class="comments-title">${renderedComments.title}</h3></div>`;
+  if (renderedComments.comments)
+    document.getElementById(
+      "comments"
+    ).innerHTML += `<ol class="ast-comment-list">${renderedComments.comments}</ol>`;
+}
 
 function postRenderer(lineArr, depth) {
-	var output = ""
-	for (let i = 0; i < lineArr.length; i++) {
-		let line = lineArr[i]
-		if (line.type == "h3") {
-			output += `<h3 class="wp-block-heading">${checkForTags(line.entries[0])}</h3>`
-		} else if (line.type == "list") {
-			output += `<${line.typeOf}l class="${line.classes}" style="${line.style}">`
-			line.entries.forEach(listItem => {
-				output += `<li>${checkForTags(listItem)}</li>`
-			});
-			output += `</${line.typeOf}l>`
-		} else {
-			output += `<p>${checkForTags(line)}</p>`
-		}
-	}
+  var output = "";
+  for (let i = 0; i < lineArr.length; i++) {
+    let line = lineArr[i];
+    if (line.type == "h3") {
+      output += `<h3 class="wp-block-heading">${checkForTags(
+        line.entries[0]
+      )}</h3>`;
+    } else if (line.type == "list") {
+      output += `<${line.typeOf}l class="${line.classes}" style="${line.style}">`;
+      line.entries.forEach((listItem) => {
+        output += `<li>${checkForTags(listItem)}</li>`;
+      });
+      output += `</${line.typeOf}l>`;
+    } else {
+      output += `<p>${checkForTags(line)}</p>`;
+    }
+  }
 
-	return output
+  return output;
 }
 
 function checkForTags(txt) {
-	output = txt
-	output = output.replaceAll(/{@b (.*)}/g, "<strong>$1</strong>") // Bold
-	output = output.replaceAll(/{@link (.*)\|(.*)}/g, "<a target=\"_blank\" href=\"$2\">$1</a>") // Links
+  output = txt;
+  output = output.replaceAll(/{@b (.*)}/g, "<strong>$1</strong>"); // Bold
+  output = output.replaceAll(
+    /{@link (.*)\|(.*)}/g,
+    '<a target="_blank" href="$2">$1</a>'
+  ); // Links
 
-	return output
+  return output;
 }
 
 function checkForNavigation(post) {
-	var output = ""
+  var output = "";
 
-	var next = globalThis.posts.find(obj => {
-		return obj.id == post.id - 1
-	})
+  var next = globalThis.posts.find((obj) => {
+    return obj.id == post.id - 1;
+  });
 
-	var previous = globalThis.posts.find(obj => {
-		return obj.id == post.id + 1
-	})
+  var previous = globalThis.posts.find((obj) => {
+    return obj.id == post.id + 1;
+  });
 
-	if (next) {
-		output += `<div class="nav-previous"><a href="https://dungeonsanddorks.github.io/blog/?post=${post.id - 1}" rel="prev"><span class="ast-left-arrow">←</span> Previous Post</a></div>`
-	}
-	
-	if (previous) {
-		output += `<div class="nav-next"><a href="https://dungeonsanddorks.github.io/blog/?post=${post.id + 1}" rel="next">Next Post <span class="ast-right-arrow">→</span></a></div>`
-	}
+  if (next) {
+    output += `<div class="nav-previous"><a href="https://dungeonsanddorks.github.io/blog/?post=${
+      post.id - 1
+    }" rel="prev"><span class="ast-left-arrow">←</span> Previous Post</a></div>`;
+  }
 
-	return output
+  if (previous) {
+    output += `<div class="nav-next"><a href="https://dungeonsanddorks.github.io/blog/?post=${
+      post.id + 1
+    }" rel="next">Next Post <span class="ast-right-arrow">→</span></a></div>`;
+  }
+
+  return output;
 }
 
-function renderComments(post) {
-	var comments = globalThis.comments.filter(obj => obj.postID == post.id)
-	
-	if (comments.length == 0) {
-		document.getElementsByClassName("entry-meta")[0].innerHTML = `<span class="comments-link"><a href="https://dungeonsanddorks.github.io/blog/?post=${post.id}#respond">Leave a Comment</a></span> /` + document.getElementsByClassName("entry-meta")[0].innerHTML;
-		return { comments: "", title: "" };
-	} else if (comments.length == 1) {
-		document.getElementsByClassName("entry-meta")[0].innerHTML = `<span class="comments-link"><a href="https://dungeonsanddorks.github.io/blog/?post=${post.id}#comments">1 Comment</a></span> /` + document.getElementsByClassName("entry-meta")[0].innerHTML;
-		return { comments: "", title: `1 thought on “${post.title}”` }
-	} else {
-		document.getElementsByClassName("entry-meta")[0].innerHTML = `<span class="comments-link"><a href="https://dungeonsanddorks.github.io/blog/?post=${post.id}#comments">${comments.length} Comments</a></span> /` + document.getElementsByClassName("entry-meta")[0].innerHTML;
-	}
-	
-	var output = ""
-	
-	return { comments: output, title: `${comments.length} thoughts on “${post.title}”` }
+function checkForComments(post) {
+  var comments = globalThis.comments.filter((obj) => obj.postID == post.id);
+
+  if (comments.length == 0) {
+    document.getElementsByClassName("entry-meta")[0].innerHTML =
+      `<span class="comments-link"><a href="https://dungeonsanddorks.github.io/blog/?post=${post.id}#respond">Leave a Comment</a></span> /` +
+      document.getElementsByClassName("entry-meta")[0].innerHTML;
+    return { comments: "", title: "" };
+  } else if (comments.length == 1) {
+    document.getElementsByClassName("entry-meta")[0].innerHTML =
+      `<span class="comments-link"><a href="https://dungeonsanddorks.github.io/blog/?post=${post.id}#comments">1 Comment</a></span> /` +
+      document.getElementsByClassName("entry-meta")[0].innerHTML;
+    return {
+      comments: renderComment(comments[0]),
+      title: `1 thought on “${post.title}”`,
+    };
+  } else {
+    document.getElementsByClassName("entry-meta")[0].innerHTML =
+      `<span class="comments-link"><a href="https://dungeonsanddorks.github.io/blog/?post=${post.id}#comments">${comments.length} Comments</a></span> /` +
+      document.getElementsByClassName("entry-meta")[0].innerHTML;
+  }
+
+  var output = "";
+  for (const comment of comments) {
+    output += renderComment(comment);
+  }
+
+  return {
+    comments: output,
+    title: `${comments.length} thoughts on “${post.title}”`,
+  };
 }
+
+function renderComment(comment) {}
 
 async function init() {
-	const queryString = window.location.search
-	const urlParams = new URLSearchParams(queryString);
-	await loadData()
-	
-	const page = urlParams.has('post') ? urlParams.get('post').split('#')[0] : 0;
-	const result = globalThis.posts.find(obj => {
-		return obj.id == page
-	})
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  await loadData();
 
-	if (result == undefined) {
-		renderPost(globalThis.posts[0])
-	} else {
-		renderPost(result)
-	}}
+  const page = urlParams.has("post") ? urlParams.get("post").split("#")[0] : 0;
+  const result = globalThis.posts.find((obj) => {
+    return obj.id == page;
+  });
 
-init()
+  if (result == undefined) {
+    renderPost(globalThis.posts[0]);
+  } else {
+    renderPost(result);
+  }
+}
+
+init();
--- blog/blog.js
+++ blog/blog.js
@@ -1,36 +1,47 @@
 class Blog {
-	async initBlogPage () {
-		await this.loadPosts()
-		this.setUpBlogPageList()
-	}
+  async initBlogPage() {
+    await this.loadPosts();
+    this.setUpBlogPageList();
+  }
 
-	async loadPosts () {
-		this.postData = await fetch('/blog/post-data/posts.json').then(response => response.json())
-		this.posts = this.postData.posts
-		this.posts.sort((a,b) => b.dateLastModified - a.dateLastModified);
-	}
+  async loadPosts() {
+    this.postData = await fetch("/blog/post-data/posts.json").then((response) =>
+      response.json()
+    );
+    this.posts = this.postData.posts;
+    this.posts.sort((a, b) => b.dateLastModified - a.dateLastModified);
+  }
 
-	setUpBlogPageList () {
-		this.output = "";
-		if (document.getElementsByClassName("uc_post_list")?.length > 0) {
-			this.posts.forEach(post => {
-				let date = new Date(post.dateLastModified * 1000)
-				this.output += `<div class="uc_post_list_box">
+  setUpBlogPageList() {
+    this.output = "";
+    if (document.getElementsByClassName("uc_post_list")?.length > 0) {
+      this.posts.forEach((post) => {
+        let date = new Date(post.dateLastModified * 1000);
+        this.output += `<div class="uc_post_list_box">
 	<div class="uc_post_list_image">
-		<a href="https://dungeonsanddorks.github.io/blog/?post=${post.id}" target="_self" title="${post.title}">
-			<img decoding="async" src="https://raw.githubusercontent.com/dungeonsanddorks/dungeonsanddorks.github.io/main/blog/post-data/images/${post.image}" alt="${post.imageAlt}" width="768" height="576">
+		<a href="https://dungeonsanddorks.github.io/blog/?post=${
+      post.id
+    }" target="_self" title="${post.title}">
+			<img decoding="async" src="https://raw.githubusercontent.com/dungeonsanddorks/dungeonsanddorks.github.io/main/blog/post-data/images/${
+        post.image
+      }" alt="${post.imageAlt}" width="768" height="576">
 		</a>
 	</div>
 	<div class="uc_post_list_content">
 		<div class="uc_post_list_content_inside">
 			<div class="uc_post_list_title">
-				<a target="_self" href="https://dungeonsanddorks.github.io/blog/?post=${post.id}" title="${post.title}">
+				<a target="_self" href="https://dungeonsanddorks.github.io/blog/?post=${
+          post.id
+        }" title="${post.title}">
 					${post.title}
 				</a>
 			</div>	
 			<div class="ue-meta-data">
 				<div class="ue-grid-item-meta-data">
-					<span class="ue-grid-item-meta-data-icon"> <i class="fas fa-calendar-week"></i></span> ${date.toLocaleString('default', { month: 'long' })} ${date.getDate()}, ${date.getFullYear()}
+					<span class="ue-grid-item-meta-data-icon"> <i class="fas fa-calendar-week"></i></span> ${date.toLocaleString(
+            "default",
+            { month: "long" }
+          )} ${date.getDate()}, ${date.getFullYear()}
 				</div>                                   
 			</div>
 			<div class="uc_post_content">
@@ -39,14 +50,15 @@ class Blog {
 		</div>
 	</div>
 </div>
-`
-			});
-			document.getElementsByClassName("uc_post_list")[0].innerHTML += this.output;
-		} else if (document.getElementsByClassName("ue_post_blocks")?.length > 0) {
-			var runFor = this.posts.length < 6 ? this.posts.length : 6
-			for (var i = 0; i < runFor; i++) {
-				let post = this.posts[i]
-				this.output += `<div class="ue_post_blocks_box">
+`;
+      });
+      document.getElementsByClassName("uc_post_list")[0].innerHTML +=
+        this.output;
+    } else if (document.getElementsByClassName("ue_post_blocks")?.length > 0) {
+      var runFor = this.posts.length < 6 ? this.posts.length : 6;
+      for (var i = 0; i < runFor; i++) {
+        let post = this.posts[i];
+        this.output += `<div class="ue_post_blocks_box">
 <div class="ue_post_blocks_image">
 			<a href="https://dungeonsanddorks.github.io/blog/?post=${post.id}" style="display:block;">
 		<img decoding="async" src="https://raw.githubusercontent.com/dungeonsanddorks/dungeonsanddorks.github.io/main/blog/post-data/images/${post.image}" alt="${post.imageAlt}" width="768" height="576"/>
@@ -68,12 +80,13 @@ class Blog {
 	</div>
 </div>
 </div>
-`
-			};
-			document.getElementsByClassName("ue_post_blocks")[0].innerHTML += this.output;
-		}
-	} 
+`;
+      }
+      document.getElementsByClassName("ue_post_blocks")[0].innerHTML +=
+        this.output;
+    }
+  }
 }
 
-const blog = new Blog
-blog.initBlogPage()
+const blog = new Blog();
+blog.initBlogPage();
