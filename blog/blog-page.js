fabb3c82c0e880533471424527671ec06c747bc2
asdfgn2399 <alexannett88@gmail.com>
2023-12-14 21:09:59 -0800

    Adding Reply (alpha)

--- blog/blog-page.js
+++ blog/blog-page.js
@@ -1,178 +1,282 @@
 async function loadData() {
-  let [posts, comments] = await Promise.all([
-    fetch("/blog/post-data/posts.json").then((response) => response.json()),
-    fetch("/blog/comment-data/comments.json").then((response) =>
-      response.json()
-    ),
-  ]);
-  globalThis.posts = posts.posts.sort(
-    (a, b) => b.dateLastModified - a.dateLastModified
-  );
-  globalThis.comments = comments.comments;
+	let [posts, comments] = await Promise.all([
+		fetch("/blog/post-data/posts.json").then((response) => response.json()),
+		fetch("/blog/comment-data/comments.json").then((response) =>
+			response.json()
+		),
+	]);
+	globalThis.posts = posts.posts.sort(
+		(a, b) => b.dateLastModified - a.dateLastModified
+	);
+	globalThis.comments = comments.comments;
 }
 
 function renderPost(post) {
-  // Render Post Header
-  var imageHTML = "";
-  if (post.image !== "placeholder.jpeg")
-    imageHTML = `<div class="post-thumb-img-content post-thumb">
+	// Render Post Header
+	var imageHTML = "";
+	if (post.image !== "placeholder.jpeg")
+		imageHTML = `<div class="post-thumb-img-content post-thumb">
 		<img width="1024" height="768" src="https://raw.githubusercontent.com/dungeonsanddorks/dungeonsanddorks.github.io/main/blog/post-data/images/${post.image}" class="attachment-large size-large wp-post-image" alt="${post.imageAlt}" itemprop="image" decoding="async" sizes="(max-width: 1024px) 100vw, 1024px">
 	</div>
 	`;
-  var output =
-    `<header class="entry-header">
+	var output =
+		`<header class="entry-header">
 	<h1 class="entry-title" itemprop="headline">${post.title}</h1>
 	<div class="entry-meta">
 	` +
-    /* TODO: Comments  */ /* TODO: Catagoreies <span class="ast-terms-link"><a href="https://dungeonsanddorks.github.io/category/uncategorized/">Uncategorized</a></span> / */ `By <span class="posted-by vcard author" itemtype="https://schema.org/Person" itemscope="itemscope" itemprop="author"><a title="View all posts by ${
-      post.author
-    }" href="https://dungeonsanddorks.github.io/author/${post.author
-      .replaceAll(" ", "-")
-      .toLowerCase()}/" rel="author" class="url fn n" itemprop="url"><span class="author-name" itemprop="name">${
-      post.author
-    }</span> </a> </span> 
+		/* TODO: Comments  */ /* TODO: Catagoreies <span class="ast-terms-link"><a href="https://dungeonsanddorks.github.io/category/uncategorized/">Uncategorized</a></span> / */ `By <span class="posted-by vcard author" itemtype="https://schema.org/Person" itemscope="itemscope" itemprop="author"><a title="View all posts by ${
+			post.author
+		}" href="https://dungeonsanddorks.github.io/author/${post.author
+			.replaceAll(" ", "-")
+			.toLowerCase()}/" rel="author" class="url fn n" itemprop="url"><span class="author-name" itemprop="name">${
+			post.author
+		}</span> </a> </span> 
 	</div>
 	${imageHTML}
 	</header>
 `;
-  document.getElementsByClassName("ast-post-format-")[0].innerHTML += output;
+	document.getElementsByClassName("ast-post-format-")[0].innerHTML += output;
 
-  // Render Post Content
-  document.getElementsByClassName("ast-post-format-")[0].innerHTML +=
-    '<div class="entry-content clear" itemprop="text">' +
-    postRenderer(post.entries, 0) +
-    "</div>";
+	// Render Post Content
+	document.getElementsByClassName("ast-post-format-")[0].innerHTML +=
+		'<div class="entry-content clear" itemprop="text">' +
+		postRenderer(post.entries, 0) +
+		"</div>";
 
-  // Render Navigation Links
-  document.getElementsByClassName("nav-links")[0].innerHTML =
-    checkForNavigation(post);
+	// Render Navigation Links
+	document.getElementsByClassName("nav-links")[0].innerHTML =
+		checkForNavigation(post);
 
-  // Render Comments
-  const renderedComments = checkForComments(post);
-  if (renderedComments.title)
-    document.getElementById(
-      "comments"
-    ).innerHTML += `<div class="comments-count-wrapper"><h3 class="comments-title">${renderedComments.title}</h3></div>`;
-  if (renderedComments.comments)
-    document.getElementById(
-      "comments"
-    ).innerHTML += `<ol class="ast-comment-list">${renderedComments.comments}</ol>`;
+	// Render Comments
+	const renderedComments = checkForComments(post);
+	if (renderedComments.title)
+		document.getElementById(
+			"comments"
+		).innerHTML += `<div class="comments-count-wrapper"><h3 class="comments-title">${renderedComments.title}</h3></div>`;
+	if (renderedComments.comments)
+		document.getElementById(
+			"comments"
+		).innerHTML += `<ol class="ast-comment-list">${renderedComments.comments}</ol>`;
+
+	document.getElementById(
+		"comments"
+	).innerHTML += `<div id="respond" class="comment-respond">
+		<h3 id="reply-title" class="comment-reply-title">
+			Leave a Comment
+			<small
+				><a
+					rel="nofollow"
+					id="cancel-comment-reply-link"
+					href="blog/?post=${post.id}#respond"
+					style="display: none"
+					>Cancel Reply</a
+				></small
+			>
+		</h3>
+		<p class="comment-notes">
+			<span id="email-notes">Your email address will not be published.</span>
+			<span class="required-field-message"
+				>Required fields are marked <span class="required">*</span></span
+			>
+		</p>
+		<div class="ast-row comment-textarea">
+			<fieldset class="comment-form-comment">
+				<legend class="comment-form-legend"></legend>
+				<div class="comment-form-textarea ast-col-lg-12">
+					<label for="comment" class="screen-reader-text">Type here..</label
+					><textarea
+						id="comment"
+						name="comment"
+						placeholder="Type here.."
+						cols="45"
+						rows="8"
+						aria-required="true"
+					></textarea>
+				</div>
+			</fieldset>
+		</div>
+		<div class="ast-comment-formwrap ast-row">
+			<p
+				class="comment-form-author ast-col-xs-12 ast-col-sm-12 ast-col-md-4 ast-col-lg-4"
+			>
+				<label for="author" class="screen-reader-text">Name*</label
+				><input
+					id="author"
+					name="author"
+					type="text"
+					value=""
+					placeholder="Name*"
+					size="30"
+					aria-required="true"
+				/>
+			</p>
+			<p
+				class="comment-form-email ast-col-xs-12 ast-col-sm-12 ast-col-md-4 ast-col-lg-4"
+			>
+				<label for="email" class="screen-reader-text">Email*</label
+				><input
+					id="email"
+					name="email"
+					type="text"
+					value=""
+					placeholder="Email*"
+					size="30"
+					aria-required="true"
+				/>
+			</p>
+			<p
+				class="comment-form-url ast-col-xs-12 ast-col-sm-12 ast-col-md-4 ast-col-lg-4"
+			>
+				<label for="url"
+					><label for="url" class="screen-reader-text">Website</label
+					><input
+						id="url"
+						name="url"
+						type="text"
+						value=""
+						placeholder="Website"
+						size="30"
+				/></label>
+			</p>
+		</div>
+		<p class="comment-form-cookies-consent">
+			<input
+				id="wp-comment-cookies-consent"
+				name="wp-comment-cookies-consent"
+				type="checkbox"
+			/>
+			<label for="wp-comment-cookies-consent"
+				>Save my name, email, and website in this browser for the next time I
+				comment.</label
+			>
+		</p>
+		<p class="form-submit">
+			<input
+				name="submit"
+				type="submit"
+				id="submit"
+				class="submit"
+				value="Post Comment »"
+			/>
+		</p>
+	</div>
+	`
 }
 
 function postRenderer(lineArr, depth) {
-  var output = "";
-  for (let i = 0; i < lineArr.length; i++) {
-    let line = lineArr[i];
-    if (line.type == "h3") {
-      output += `<h3 class="wp-block-heading">${checkForTags(
-        line.entries[0]
-      )}</h3>`;
-    } else if (line.type == "list") {
-      output += `<${line.typeOf}l class="${line.classes}" style="${line.style}">`;
-      line.entries.forEach((listItem) => {
-        output += `<li>${checkForTags(listItem)}</li>`;
-      });
-      output += `</${line.typeOf}l>`;
-    } else {
-      output += `<p>${checkForTags(line)}</p>`;
-    }
-  }
+	var output = "";
+	for (let i = 0; i < lineArr.length; i++) {
+		let line = lineArr[i];
+		if (line.type == "h3") {
+			output += `<h3 class="wp-block-heading">${checkForTags(
+				line.entries[0]
+			)}</h3>`;
+		} else if (line.type == "list") {
+			output += `<${line.typeOf}l class="${line.classes}" style="${line.style}">`;
+			line.entries.forEach((listItem) => {
+				output += `<li>${checkForTags(listItem)}</li>`;
+			});
+			output += `</${line.typeOf}l>`;
+		} else {
+			output += `<p>${checkForTags(line)}</p>`;
+		}
+	}
 
-  return output;
+	return output;
 }
 
 function checkForTags(txt) {
-  output = txt;
-  output = output.replaceAll(/{@b (.*)}/g, "<strong>$1</strong>"); // Bold
-  output = output.replaceAll(
-    /{@link (.*)\|(.*)}/g,
-    '<a target="_blank" href="$2">$1</a>'
-  ); // Links
+	output = txt;
+	output = output.replaceAll(/{@b (.*)}/g, "<strong>$1</strong>"); // Bold
+	output = output.replaceAll(
+		/{@link (.*)\|(.*)}/g,
+		'<a target="_blank" href="$2">$1</a>'
+	); // Links
 
-  return output;
+	return output;
 }
 
 function checkForNavigation(post) {
-  var output = "";
+	var output = "";
 
-  var next = globalThis.posts.find((obj) => {
-    return obj.id == post.id - 1;
-  });
+	var next = globalThis.posts.find((obj) => {
+		return obj.id == post.id - 1;
+	});
 
-  var previous = globalThis.posts.find((obj) => {
-    return obj.id == post.id + 1;
-  });
+	var previous = globalThis.posts.find((obj) => {
+		return obj.id == post.id + 1;
+	});
 
-  if (next) {
-    output += `<div class="nav-previous"><a href="https://dungeonsanddorks.github.io/blog/?post=${
-      post.id - 1
-    }" rel="prev"><span class="ast-left-arrow">←</span> Previous Post</a></div>`;
-  }
+	if (next) {
+		output += `<div class="nav-previous"><a href="https://dungeonsanddorks.github.io/blog/?post=${
+			post.id - 1
+		}" rel="prev"><span class="ast-left-arrow">←</span> Previous Post</a></div>`;
+	}
 
-  if (previous) {
-    output += `<div class="nav-next"><a href="https://dungeonsanddorks.github.io/blog/?post=${
-      post.id + 1
-    }" rel="next">Next Post <span class="ast-right-arrow">→</span></a></div>`;
-  }
+	if (previous) {
+		output += `<div class="nav-next"><a href="https://dungeonsanddorks.github.io/blog/?post=${
+			post.id + 1
+		}" rel="next">Next Post <span class="ast-right-arrow">→</span></a></div>`;
+	}
 
-  return output;
+	return output;
 }
 
 function checkForComments(post) {
-  var totalComments = globalThis.comments.filter((obj) => obj.postID == post.id);
-  var isPural = totalComments.length > 1 ? "s" : ""
-  var topComments = globalThis.comments.filter((obj) => obj.postID == post.id && obj.depth == 1);
+	var totalComments = globalThis.comments.filter((obj) => obj.postID == post.id);
+	var isPural = totalComments.length > 1 ? "s" : ""
+	var topComments = globalThis.comments.filter((obj) => obj.postID == post.id && obj.depth == 1);
 
-  if (topComments.length == 0) {
-    document.getElementsByClassName("entry-meta")[0].innerHTML =
-      `<span class="comments-link"><a href="https://dungeonsanddorks.github.io/blog/?post=${post.id}#respond">Leave a Comment</a></span> /` +
-      document.getElementsByClassName("entry-meta")[0].innerHTML;
-    return { comments: "", title: "" };
-  } else if (topComments.length == 1) {
-    document.getElementsByClassName("entry-meta")[0].innerHTML =
-      `<span class="comments-link"><a href="https://dungeonsanddorks.github.io/blog/?post=${post.id}#comments">${totalComments.length} Comment${isPural}</a></span> /` +
-      document.getElementsByClassName("entry-meta")[0].innerHTML;
-    return {
-      comments: renderComment(topComments[0]),
-      title: `${totalComments.length} thought${isPural} on “${post.title}”`,
-    };
-  } else {
-    document.getElementsByClassName("entry-meta")[0].innerHTML =
-      `<span class="comments-link"><a href="https://dungeonsanddorks.github.io/blog/?post=${post.id}#comments">${totalComments.length} Comments</a></span> /` +
-      document.getElementsByClassName("entry-meta")[0].innerHTML;
-  }
+	if (topComments.length == 0) {
+		document.getElementsByClassName("entry-meta")[0].innerHTML =
+			`<span class="comments-link"><a href="https://dungeonsanddorks.github.io/blog/?post=${post.id}#respond">Leave a Comment</a></span> /` +
+			document.getElementsByClassName("entry-meta")[0].innerHTML;
+		return { comments: "", title: "" };
+	} else if (topComments.length == 1) {
+		document.getElementsByClassName("entry-meta")[0].innerHTML =
+			`<span class="comments-link"><a href="https://dungeonsanddorks.github.io/blog/?post=${post.id}#comments">${totalComments.length} Comment${isPural}</a></span> /` +
+			document.getElementsByClassName("entry-meta")[0].innerHTML;
+		return {
+			comments: renderComment(topComments[0]),
+			title: `${totalComments.length} thought${isPural} on “${post.title}”`,
+		};
+	} else {
+		document.getElementsByClassName("entry-meta")[0].innerHTML =
+			`<span class="comments-link"><a href="https://dungeonsanddorks.github.io/blog/?post=${post.id}#comments">${totalComments.length} Comments</a></span> /` +
+			document.getElementsByClassName("entry-meta")[0].innerHTML;
+	}
 
-  var output = "";
-  for (const comment of topComments) {
-    output += renderComment(comment);
-  }
+	var output = "";
+	for (const comment of topComments) {
+		output += renderComment(comment);
+	}
 
-  return {
-    comments: output,
-    title: `${topComments.length} thoughts on “${post.title}”`,
-  };
+	return {
+		comments: output,
+		title: `${topComments.length} thoughts on “${post.title}”`,
+	};
 }
 
 function renderComment(comment) {
-  var moreComments = globalThis.comments.filter((obj) => obj.postID == comment.postID && obj.depth == comment.depth + 1 && obj.replyToID == comment.commentID);
-  var moreCommentTxt = ''
-  if (moreComments.length > 0) {
-    moreCommentTxt = '<ol class="children">'
-    for (const comment of moreComments) {
-      moreCommentTxt += renderComment(comment)
-    }
-    moreCommentTxt += '</ol>'
-  }
+	var moreComments = globalThis.comments.filter((obj) => obj.postID == comment.postID && obj.depth == comment.depth + 1 && obj.replyToID == comment.commentID);
+	var moreCommentTxt = ''
+	if (moreComments.length > 0) {
+		moreCommentTxt = '<ol class="children">'
+		for (const comment of moreComments) {
+			moreCommentTxt += renderComment(comment)
+		}
+		moreCommentTxt += '</ol>'
+	}
 
 	var avatar = comment.avatar == "default" ? "c5653504f48da574115fdf053f96db62.png" : comment.avatar;
 	
-  var date = new Date(comment.datePosted * 1000);
-  var hours = date.getHours() % 12 || 12
-  var meridiem = date.getHours() > 12 ? "pm" : "am"
-  var time = `${date.toLocaleString(
-    "default",
-    { month: "long" }
-  )} ${date.getDate()}, ${date.getFullYear()} at ${hours}:${date.getMinutes()} ${meridiem}`
+	var date = new Date(comment.datePosted * 1000);
+	var hours = date.getHours() % 12 || 12
+	var meridiem = date.getHours() > 12 ? "pm" : "am"
+	var time = `${date.toLocaleString(
+		"default",
+		{ month: "long" }
+	)} ${date.getDate()}, ${date.getFullYear()} at ${hours}:${date.getMinutes()} ${meridiem}`
 
 	var output = "<p>"
 	for (let i = 0; i < comment.comment.length; i++) {
@@ -180,7 +284,7 @@ function renderComment(comment) {
 	}
 	output = output.substring(0, output.length - 4) + "</p>"
 
-  return `<li class="comment even thread-even depth-${comment.depth}" id="li-comment-${comment.commentID}">
+	return `<li class="comment even thread-even depth-${comment.depth}" id="li-comment-${comment.commentID}">
 	<article id="comment-${comment.commentID}" class="ast-comment">
 		<div class="ast-comment-info">
 			<div class="ast-comment-avatar-wrap">
@@ -218,25 +322,25 @@ function renderComment(comment) {
 			</section> <!-- .ast-comment-content -->
 		</div>
 	</article><!-- #comment-## -->
-  ${moreCommentTxt}
+	${moreCommentTxt}
 </li><!-- #comment-## -->`;
 }
 
 async function init() {
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
-  await loadData();
+	const queryString = window.location.search;
+	const urlParams = new URLSearchParams(queryString);
+	await loadData();
 
-  const page = urlParams.has("post") ? urlParams.get("post").split("#")[0] : 0;
-  const result = globalThis.posts.find((obj) => {
-    return obj.id == page;
-  });
+	const page = urlParams.has("post") ? urlParams.get("post").split("#")[0] : 0;
+	const result = globalThis.posts.find((obj) => {
+		return obj.id == page;
+	});
 
-  if (result == undefined) {
-    renderPost(globalThis.posts[0]);
-  } else {
-    renderPost(result);
-  }
+	if (result == undefined) {
+		renderPost(globalThis.posts[0]);
+	} else {
+		renderPost(result);
+	}
 }
 
 init();
