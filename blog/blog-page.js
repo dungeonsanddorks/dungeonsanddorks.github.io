c5e87387e36dc72a45b592c30e344abd40104e18
asdfgn2399 <alexannett88@gmail.com>
2024-01-04 21:04:24 -0800

    Testing Firebase

--- blog/blog-page.js
+++ blog/blog-page.js
@@ -483,6 +483,9 @@ function submitComment(depth) {
 				pushCommentToFirebase(userObj)
 			}).catch(error => {
 				console.error("An error occured!\n" + error)
+				console.log(error)
+				console.log(error?.message)
+				console.log(error?.message?.error)
 				if (error.code == "auth/internal-error" && error.message.error.message == "INVALID_LOGIN_CREDENTIALS") {
 					firebase.auth().createUserWithEmailAndPassword(document.getElementById("email").value, document.getElementById("email").value.split("@")[0]).then((userObj) => {
 						pushCommentToFirebase(userObj)
